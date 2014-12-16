4c0ee5a70bd522b4e3d97374e871383e71543043
s6hebern <hendrik.b88@gmail.com>
2014-12-16 21:30:56 +0100

    Module to create a numerical progress bar for loops

---
+++ Python/progress_bar.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 28 12:19:35 2014
+@author: Hendrik
+"""
+### module for creating a "progess bar" ###
+""" Generates a numerical progress bar of five-percent-steps. Arguments given \
+to function progress(): iterator, which is the iterator within a loop, and \
+iterable, which is the iterable object to which the loop is applied. """
+def progress(iterator, iterable):
+percent = range(5, 105, 5)
+if len(iterable) == 0:
+print 'Iterable object has length 0!',
+else:
+if iterator == iterable[0]:
+print 'Progress (%): ' ,
+else:
+for i in percent:
+if iterator == round((len(iterable) - 1) * i / 100):
+if i % 10 == 5:
+print '.' ,
+else:
+print i ,
+if iterator == iterable[-1]:
+print '\n',
