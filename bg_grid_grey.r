1e1735300808d8fe1e57d1f416896d88636a549f
s6hebern <hendrik.b88@gmail.com>
2014-12-16 18:52:01 +0100

    change background colour to grey and draw grid

--- bg_grid_grey.r
+++ bg_grid_grey.r
@@ -3,15 +3,18 @@
 # Also changes the background colour of the plot to lightgrey.
 
 bg_grid_grey <- function() {
+  # set background colour of the plotting region:
     rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "lightgrey", border=NA)
+  # draw grid at major tickmarks
+    grid(lty=1, col="white")
+  # draw vertical lines at minor x-tickmarks:
     for (v in seq(par("xaxp")[1], par("xaxp")[2] + par("xaxp")[2]/(par("xaxp")[3]*2), 
                   par("xaxp")[2]/(par("xaxp")[3]*2))) {
       abline(v=v, col="grey90")
     }
-    
+  # draw horizontal lines at minor y-tickmarks:
     for (h in seq(par("yaxp")[1], par("yaxp")[2] + par("yaxp")[2]/(par("yaxp")[3]*2), 
                   par("yaxp")[2]/(par("yaxp")[3]*2))) {
       abline(h=h, col="grey90")
     }
-    grid(lty=1, col="white")
 }
