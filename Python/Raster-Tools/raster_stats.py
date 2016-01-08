a0f28e64104865bdb3b3baf39c2fbc3fe762b5d7
s6hebern <hendrik.b88@gmail.com>
2016-01-08 20:18:48 +0100

    Functions for basic raster-statistic computation

--- Python/Raster-Tools/raster_stats.py
+++ Python/Raster-Tools/raster_stats.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os, sys
 from osgeo import gdal
 from osgeo.gdalconst import *
 import numpy as np
+import csv
 
 def calc_stat_raster(infile, outfile, mode, of="GTiff", co=None):
     
@@ -54,10 +56,10 @@ def calc_stat_raster(infile, outfile, mode, of="GTiff", co=None):
         num_bands = len(mode)
     
     if co == None:
-        ds_out = driver.Create(out, ds.RasterXSize, ds.RasterYSize, num_bands, \
+        ds_out = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, num_bands, \
                 band.DataType)
     else:
-        ds_out = driver.Create(out, ds.RasterXSize, ds.RasterYSize, num_bands, \
+        ds_out = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, num_bands, \
                 band.DataType, co)
     
     ds_out.SetProjection(ds.GetProjection())
@@ -73,7 +75,6 @@ def calc_stat_raster(infile, outfile, mode, of="GTiff", co=None):
             stat = getattr(np, mode[b - 1])
 
         data = stat(ds_arr, axis=0)
-        
         # write data:
         b_out.WriteArray(data)
         # set band name to statistical value(s):
@@ -86,3 +87,94 @@ def calc_stat_raster(infile, outfile, mode, of="GTiff", co=None):
     
     ds_out = None
     ds = None
+
+
+def histogram(infile, band=1, outfile=None, delim=','):
+    
+    """
+    Compute the exact histogram of an 8bit image, which can be written into a
+    csv file containing all values and counts within the min-max-range.
+    
+    Use:
+    
+    infile (string): the input image (full path and file extension). May 
+            contain multiple bands
+    
+    band (integer): the number of the image band from which the histogram shall
+            be computed. Counting starts at 1. Defaults to 1.
+    
+    outfile (string): the output file (full path and file extension), if the
+            histogram shall be written into a csv file. Defaults to None, which
+            means that no output file is created. Nonetheless, the histogram 
+            itself is returned.
+    
+    delim (string): the delimiter to use for the csv file. Defaults to ",".
+    """
+    
+    b_index = band
+    # read file:
+    ds = gdal.Open(infile, GA_ReadOnly)
+    band = ds.GetRasterBand(b_index)
+    # get min/max and compute exact histogram:
+    minmax = band.ComputeRasterMinMax()
+    hist = band.GetHistogram(approx_ok=0)
+    
+    # write histogram to csv, if desired:
+    if outfile != None:
+        values = [i for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+        counts = [hist[i] for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+        # create csv
+        with open(outfile, 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=delim)
+            writer.writerow(['Value', 'Count'])
+            writer.writerows(zip(values, counts))
+    # return histogram for further analysis:
+    return(hist)
+
+
+def area_per_class(infile, outfile, band=1, delim=','):
+    
+    """
+    Compute the per-class-area from a classification image and create a csv 
+            table from it.
+    
+    Use:
+    
+    infile (string): the input image (full path and file extension). May 
+            contain multiple bands
+    
+    outfile (string): the output file (full path and file extension), if the
+            histogram shall be written into a csv file. Defaults to None, which
+            means that no output file is created. Nonetheless, the histogram 
+            itself is returned.
+    
+    band (integer): the number of the image band from which the histogram shall
+            be computed. Counting starts at 1. Defaults to 1.
+            
+    delim (string): the delimiter to use for the csv file. Defaults to ",".
+    """
+    
+    b_index = band
+    # read file:
+    ds = gdal.Open(infile, GA_ReadOnly)
+    band = ds.GetRasterBand(b_index)
+    # get raster metrics:
+    reso = ds.GetGeoTransform()[1]
+    
+    # get histogram (using function "histogram"):
+    hist = histogram(infile, b_index)
+    minmax = band.ComputeRasterMinMax()
+    values = [i for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+    counts = [hist[i] for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+    
+    # calculate area per class (apc):
+    apc = [i * (reso * reso) for i in counts]
+    
+    # write csv:
+    values = [i for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+    counts = [hist[i] for i in xrange(int(minmax[0]), int(minmax[1]) + 1)]
+    # create csv
+    with open(outfile, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=delim)
+        writer.writerow(['Value', 'Count', 'Area'])
+        writer.writerows(zip(values, counts, apc))
