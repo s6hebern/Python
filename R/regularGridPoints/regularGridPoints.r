00bb3051e6c051505f733d1dd02961d2b848ca0f
s6hebern <hendrik.b88@gmail.com>
2017-02-26 15:12:32 +0100

    Update regularGridPoints.r

--- R/regularGridPoints/regularGridPoints.r
+++ R/regularGridPoints/regularGridPoints.r
@@ -1,10 +1,12 @@
-regularGridPoints <- function(xmin, ymin, xmax, ymax, stepx, stepy, out, epsgIn="4326", stepDeg=F, epsgOut="4326") {
+regularGridPoints <- function(xmin, ymin, xmax, ymax, stepx, stepy, outfile, 
+                              epsgIn="4326", stepDeg=F, epsgOut="4326", show=F) {
+
   #-------------------------------------------------------------------------#
   # check for required package "rgdal", install if necessary
-  list.of.packages <- c("rgdal")
+  list.of.packages <- c("rgdal", "ggmap")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
-  library(rgdal)
+  require(rgdal, ggmap)
   #-------------------------------------------------------------------------#
   
   xmin_in <- xmin
@@ -34,83 +36,80 @@ regularGridPoints <- function(xmin, ymin, xmax, ymax, stepx, stepy, out, epsgIn=
   ymin_out <- as.numeric(XY_out[1]$Y)
   xmax_out <- as.numeric(XY_out[2]$X)
   ymax_out <- as.numeric(XY_out[2]$Y)
-
+  
   #############################################################################
   # create list of necessary x- and y-values
   #############################################################################
-    
+  
   # if input and output CRS are in degrees
-  if(epsgIn == "4326" & epsgOut == "4326") {
+  if (epsgIn == "4326" & epsgOut == "4326") {
     #-------------------------------------------------------------------------#
-    # if steps are degrees
-    if (stepDeg == T) {
-      # calculate all possible distinct x and y values (in degrees)
-      xvals <- seq(xmin_out, xmax_out, by=stepx)
-      yvals <- seq(ymin_out, ymax_out, by=stepy)
-    } else { # steps are in meters
-      # calculate UTM zone
-      utm_zone <- as.character((floor((mean(c(xmin_in, xmax_in)) + 180)/6) %% 60) + 1)
-      crs_utm <- CRS(paste("+init=epsg:326", utm_zone, sep=""))
-      # transform input coordinates to UTM
-      XY_utm <- spTransform(XY_in, CRSobj=crs_utm)
-      recent_crs <- crs_utm
-      # extract temporary corner coordinates (now in UTM)
-      xmin_utm <- as.numeric(XY_utm[1]$X)
-      ymin_utm <- as.numeric(XY_utm[1]$Y)
-      xmax_utm <- as.numeric(XY_utm[2]$X)
-      ymax_utm <- as.numeric(XY_utm[2]$Y)
-      # calculate the number of all possible distinct x- and y-values (in UTM)
-      xcount <- length(seq(xmin_utm, xmax_utm, by=stepx))
-      ycount <- length(seq(ymin_utm, ymax_utm, by=stepy))
-      # calculate all possible distinct x and y values (in degrees)
-      xvals <- seq(xmin_out, xmax_out, length.out=xcount)
-      yvals <- seq(ymin_out, ymax_out, length.out=ycount)
-    }
+    # if steps are meters
+    if (stepDeg == F) {
+        # calculate UTM zone
+        utm_zone <- as.character((floor((mean(c(xmin_in, xmax_in)) + 180)/6) %% 60) + 1)
+        print(paste("Transforming to UTM, Zone ", as.character(utm_zone), "N", sep=""))
+        crs_utm <- CRS(paste("+init=epsg:326", utm_zone, sep=""))
+        # transform input coordinates to UTM
+        XY_utm <- spTransform(XY_in, CRSobj=crs_utm)
+        crs_temp <- crs_utm
+        # extract temporary corner coordinates (now in UTM)
+        xmin_utm <- as.numeric(XY_utm[1]$X)
+        ymin_utm <- as.numeric(XY_utm[1]$Y)
+        xmax_utm <- as.numeric(XY_utm[2]$X)
+        ymax_utm <- as.numeric(XY_utm[2]$Y)
+        # calculate all possible distinct x and y values (in UTM)
+        xvals <- seq(xmin_utm, xmax_utm, by=stepx)
+        yvals <- seq(ymin_utm, ymax_utm, by=stepy)
+      } else { # steps are degrees
+        # calculate all possible distinct x and y values (in degrees)
+        xvals <- seq(xmin_in, xmax_in, by=stepx)
+        yvals <- seq(ymin_in, ymax_in, by=stepy)
+        crs_temp <- crs_in
+      }
     #-------------------------------------------------------------------------#
   } else if (epsgIn == "4326" & epsgOut != "4326") { # input CRS is geographic, output CRS it other
     #-------------------------------------------------------------------------#
-    # if steps are in degrees
-    if (stepDeg == T) {
-      # calculate the number of all possible distinct x- and y-values (in degrees)
-      xcount <- length(seq(xmin_in, xmax_in, by=stepx))
-      ycount <- length(seq(ymin_in, ymax_in, by=stepy))
-      # calculate all possible distinct x and y values (in desired CRS)
-      xvals <- seq(xmin_out, xmax_out, length.out=xcount)
-      yvals <- seq(ymin_out, ymax_out, length.out=ycount)
-    } else { #steps are in meters
+    # if steps are in meters
+    if (stepDeg == F) {
+      # calculate all possible distinct x and y values (in output CRS)
+      xvals <- seq(xmin_out, xmax_out, by=stepx)
+      yvals <- seq(ymin_out, ymax_out, by=stepy)
+      crs_temp <- crs_out
+    } else { # steps are in degrees
+      # calculate all possible distinct x and y values (in degrees)
+      xvals <- seq(xmin_in, xmax_in, by=stepx)
+      yvals <- seq(ymin_in, ymax_in, by=stepy)
+      crs_temp <- crs_in
+    }
+    #-------------------------------------------------------------------------#
+  } else if (epsgIn != "4326" & epsgOut != "4326") { # both CRS are metric
+    #-------------------------------------------------------------------------#
+    # if steps are in meters
+    if (stepDeg == F) {
       # calculate all possible distinct x and y values (in meters)
       xvals <- seq(xmin_out, xmax_out, by=stepx)
       yvals <- seq(ymin_out, ymax_out, by=stepy)
-    }
+      crs_temp <- crs_out
+      } else { # steps in degrees --> throw error message
+        stop("If neither your input nor your output coordinate systems are 
+           geographic (EPSG: 4326), you can't use stepDeg=TRUE! Please set 
+           it to FALSE and provide stepx and stepy in meters!")
+      }
     #-------------------------------------------------------------------------#
-  } else if (epsgIn != "4326" & epsgOut != "4326") {
+  } else if (epsgIn != "4326" & epsgOut == "4326") { # input CRS is metric, output CRS it geographic
     #-------------------------------------------------------------------------#
-    # if steps are in degrees, throw error message
-    if (stepDeg == T) {
-      stop("If neither your input nor your output coordinate systems are geographic (EPSG: 4326), you can't use stepDeg=TRUE! Please set it to FALSE and provide stepx and stepy in meters!")
-    } else {
-      # calculate all possible distinct x and y values (in meters)
+    # if steps are in meters
+    if (stepDeg == F) {
+      # calculate all possbile distinct x and y values (in neters)
+      xvals <- seq(xmin_in, xmax_in, by=stepx)
+      yvals <- seq(ymin_in, ymax_in, by=stepy)
+      crs_temp <- crs_in
+    } else { # steps are in degrees
+      # calculate all possible distinct x and y values (in degrees)
       xvals <- seq(xmin_out, xmax_out, by=stepx)
       yvals <- seq(ymin_out, ymax_out, by=stepy)
-    }
-    #-------------------------------------------------------------------------#
-  } else if (epsgIn != "4326" & epsgOut == "4326") {
-    #-------------------------------------------------------------------------#
-    # if steps are in degrees
-    if (stepDeg == T) {
-      # calculate the number of all possible distinct x- and y-values (in degrees)
-      xcount <- length(seq(xmin_out, xmax_out, by=stepx))
-      ycount <- length(seq(ymin_out, ymax_out, by=stepy))
-      # calculate all possible distinct x and y values (in desired CRS)
-      xvals <- seq(xmin_out, xmax_out, length.out=xcount)
-      yvals <- seq(ymin_out, ymax_out, length.out=ycount)
-    } else {
-      # calculate the number of all possible distinct x- and y-values (in meters)
-      xcount <- length(seq(xmin_in, xmax_in, by=stepx))
-      ycount <- length(seq(ymin_in, ymax_in, by=stepy))
-      # calculate all possible distinct x and y values (in desired CRS)
-      xvals <- seq(xmin_out, xmax_out, length.out=xcount)
-      yvals <- seq(ymin_out, ymax_out, length.out=ycount)
+      crs_temp <- crs_out
     }
     #-------------------------------------------------------------------------#
   }
@@ -130,15 +129,44 @@ regularGridPoints <- function(xmin, ymin, xmax, ymax, stepx, stepy, out, epsgIn=
   flatgrid <- data.frame(xlist, ylist)
   # make SpatialPointsDataFrame and provide coordinate columns
   coordinates(flatgrid) <- c("xlist", "ylist")
-  # assign output CRS
-  proj4string(flatgrid) <- crs_out
+  # assign current CRS
+  proj4string(flatgrid) <- crs_temp
+  epsg_temp <- substr(CRSargs(crs_temp), 
+                      as.numeric(regexpr("epsg:", CRSargs(crs_temp)))+5, 
+                      as.numeric(regexpr("+proj", CRSargs(crs_temp)))-3)
+  # if another transform is needed to match output CRS
+  if (epsg_temp != epsgOut) {
+    flatgrid <- spTransform(flatgrid, crs_out)
+  }
   # add ID column
   flatgrid <- as.data.frame(flatgrid)
+  # calculate total number of points for ID
   coordcount <- length(xvals) * length(yvals)
   print(paste("Total number of points created:", coordcount))
+  # fill ID column
   flatgrid <- cbind(seq(1, coordcount), flatgrid)
+  # set new column names
   colnames(flatgrid) <- c("ID", "X", "Y")
   # export to csv
-  write.table(flatgrid, file=out, sep=",", row.names=F)
-  print(paste("Output written to:", out))
+  write.table(flatgrid, file=outfile, sep=",", row.names=F)
+  print(paste("Output written to:", outfile))
+  
+  #############################################################################
+  # plot map if desired
+  if (show == T) {
+    print("Drawing map...")
+    mapData <- flatgrid
+    coordinates(mapData) <- c("X", "Y")
+    proj4string(mapData) <- crs_out
+    if (epsgOut != "4326") {
+      mapData <- spTransform(mapData, CRS("+init=epsg:4326"))
+    }
+    mapData <- as.data.frame(mapData)
+    # get initial map
+    map <- get_map(location=make_bbox(lon=X, lat=Y, data=mapData), zoom=calc_zoom(lon=X, lat=Y, data=mapData)-2,
+                   maptype="terrain", source="google", color="bw")
+    # plot webmap and add points
+    ggmap(map, maprange=F) +
+      geom_point(aes(x=X, y=Y), data=mapData, color="red", size=2)
+  }
 }
