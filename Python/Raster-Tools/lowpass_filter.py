2b8ad2461abab36d68f56e6719f0754f864f178a
s6hebern <hendrik.b88@gmail.com>
2017-05-29 09:05:36 +0200

    support multiple raster bands

--- Python/Raster-Tools/lowpass_filter.py
+++ Python/Raster-Tools/lowpass_filter.py
@@ -5,12 +5,13 @@
 import os, sys, time, datetime, gdal, string
 import numpy as np
 from gdalconst import *
+from osgeo import gdal_array
 from optparse import OptionParser, OptionGroup
 
 __version__ = 1.0
 
 # lowpass-filter for integer images
-def lowpass_filter(img_in, img_out, window, mode='mean', band=1, of='GTiff', co=None):
+def lowpass_filter(img_in, img_out, window, mode='mean', bands=None, of='GTiff', co=None):
     """
     Apply a lowpass filter to an (integer) image.
 
@@ -18,7 +19,9 @@ def lowpass_filter(img_in, img_out, window, mode='mean', band=1, of='GTiff', co=
 
     :param string img_in: Input image (full path and file extension). May contain
             multiple bands.
-    :param string img_out: the output image (full path and file extension).
+    :param string img_out: the output image (full path and file extension). Will
+            have the same number of bands as the input image unless stated otherwise
+            with parameter "bands".
     :param integer window: the size of the filter window. 3 means a filter size of
             3x3, 5 means 5x5, etc.
             IMPORTANT NOTE: the output image will be smaller than the input
@@ -30,8 +33,8 @@ def lowpass_filter(img_in, img_out, window, mode='mean', band=1, of='GTiff', co=
             - "min"
             - "max"
             NAs are omitted in any case.
-    :param integer band: (optional) the desired band of the input image which shall be filtered.
-            Counting starts at 1 (default).
+    :param list bands: (optional) list of integers, naming the desired bands of the input
+            image to be filtered. Defaults to all bands. Counting starts at 1.
     :param string of: (optional) the desired format of the output file as provided by the
             GDAL raster formats (see: http://www.gdal.org/formats_list.html).
             Defaults to 'GTiFF'.
@@ -43,7 +46,6 @@ def lowpass_filter(img_in, img_out, window, mode='mean', band=1, of='GTiff', co=
 
     # set up input correctly
     window = int(window)
-    band = int(band)
 
     # register all of the GDAL drivers
     gdal.AllRegister()
@@ -59,61 +61,72 @@ def lowpass_filter(img_in, img_out, window, mode='mean', band=1, of='GTiff', co=
     rows = inDs.RasterYSize
     cols = inDs.RasterXSize
 
-    # read the input data
-    inBand = inDs.GetRasterBand(band)
-    inData = inBand.ReadAsArray(0, 0, cols, rows).astype(np.int)
-
-    # initiate output array
-    outData = np.zeros((rows, cols), np.int)
-
-    # do the calculation with array slices:
-    rev = list(reversed(range(window)))
-    # exp = '('
-    exp = string.join(['np.nan', mode, '(['], sep='')
-
-    # set up string command, that will later be evaluated
-    for i in range(window):
-        for j in rev:
-            exp = string.join(
-                [exp, 'inData[', str(i), ':rows-', str(rev[i]), ', ', str(rev[j]), \
-                 ':cols-', str(j), '], '], sep='')
-
-    exp = string.join([exp[:-2], '], axis=0)'], sep='')
-
-    # evaluate string expression and fill outData with it
-    print 'Filtering image %s with a %sx%s &s window...' %(os.path.basename(img_in), window, window, mode)
-    outData[(window/2):rows-(window/2), (window/2):cols-(window/2)] = eval(exp)
-
-    # for a window of 3x3 for a filter, exp would look like this:
-    # outData[1:rows-1,1:cols-1] = np.nanmean(inData[0:rows-2, 0:cols-2] + \
-    #                                           inData[0:rows-2, 1:cols-1] + \
-    #                                           inData[0:rows-2, 2:cols-0] + \
-    #                                           inData[1:rows-1, 0:cols-2] + \
-    #                                           inData[1:rows-1, 1:cols-1] + \
-    #                                           inData[1:rows-1, 2:cols-0] + \
-    #                                           inData[2:rows-0, 0:cols-2] + \
-    #                                           inData[2:rows-0, 1:cols-1] + \
-    #                                           inData[2:rows-0, 2:cols-0])
-
     # create the output image
     print 'Creating output image %s' %img_out
+
     driver = gdal.GetDriverByName(of)
-    if co == None:
-        outDs = driver.Create(img_out, cols, rows, 1, inBand.DataType)
+    if bands == None:
+        bands = range(1, inDs.RasterCount + 1)
     else:
-        outDs = driver.Create(img_out, cols, rows, 1, inBand.DataType, co)
+        bands = [int(b) for b in bands]
+
+    dtype = inDs.GetRasterBand(1).DataType
+    np_dtype = gdal_array.GDALTypeCodeToNumericTypeCode(dtype)
+
+    if co == None:
+        outDs = driver.Create(img_out, cols, rows, len(bands), dtype)
+    else:
+        outDs = driver.Create(img_out, cols, rows, len(bands), dtype, co)
     if outDs is None:
       print 'Could not create &s' %img_out
       sys.exit(1)
 
-    outBand = outDs.GetRasterBand(1)
+    # read the input data
+    print 'Filtering image %s with a %sx%s %s window...' % (os.path.basename(img_in), window, window, mode)
 
-    # write the output data
-    outBand.WriteArray(outData, 0, 0)
+    for band in bands:
+        inBand = inDs.GetRasterBand(band)
+        inData = inBand.ReadAsArray(0, 0, cols, rows).astype(np_dtype)
 
-    # flush data to disk, set the NoData value and calculate stats
-    outBand.FlushCache()
-    stats = outBand.GetStatistics(0, 1)
+        # initiate output array
+        outData = np.zeros((rows, cols), np_dtype)
+
+        # do the calculation with array slices:
+        rev = list(reversed(range(window)))
+        # exp = '('
+        exp = string.join(['np.nan', mode, '(['], sep='')
+
+        # set up string command, that will later be evaluated
+        for i in range(window):
+            for j in rev:
+                exp = string.join(
+                    [exp, 'inData[', str(i), ':rows-', str(rev[i]), ', ', str(rev[j]), \
+                     ':cols-', str(j), '], '], sep='')
+
+        exp = string.join([exp[:-2], '], axis=0)'], sep='')
+
+        # evaluate string expression and fill outData with it
+        outData[(window/2):rows-(window/2), (window/2):cols-(window/2)] = eval(exp)
+
+        # for a window of 3x3 for a filter, exp would look like this:
+        # outData[1:rows-1,1:cols-1] = np.nanmean(inData[0:rows-2, 0:cols-2] + \
+        #                                           inData[0:rows-2, 1:cols-1] + \
+        #                                           inData[0:rows-2, 2:cols-0] + \
+        #                                           inData[1:rows-1, 0:cols-2] + \
+        #                                           inData[1:rows-1, 1:cols-1] + \
+        #                                           inData[1:rows-1, 2:cols-0] + \
+        #                                           inData[2:rows-0, 0:cols-2] + \
+        #                                           inData[2:rows-0, 1:cols-1] + \
+        #                                           inData[2:rows-0, 2:cols-0])
+
+        outBand = outDs.GetRasterBand(band)
+
+        # write the output data
+        outBand.WriteArray(outData, 0, 0)
+
+        # flush data to disk, set the NoData value and calculate stats
+        outBand.FlushCache()
+        stats = outBand.GetStatistics(0, 1)
 
     # georeference the image and set the projection
     outDs.SetGeoTransform(inDs.GetGeoTransform())
@@ -133,7 +146,7 @@ def run():
 
     group = OptionGroup(parser, 'Optional Arguments', '')
     group.add_option('-m', '--mode', dest='mode', default='mean', help='<string> The desired statistical mode for the filter. One of: \n - "mean" \n - "median" \n - "min" \n -"max". NAs are omitted in any case.')
-    group.add_option('-b', '--band', dest='band', default=1, help='<integer> The desired band of the input image which shall be filtered')
+    group.add_option('-b', '--bands', dest='bands', default=None, help='<sequence of integers> The desired bands of the input image which shall be filtered. Output will have this number of bands. Defaults to all bands')
     group.add_option('-f', '--out_format', dest='of', default='GTiff', help='<string> File format of output image')
     group.add_option('-c', '--create_options', dest='co', help='<sequence of strings> Advanced raster creation options, such as band interleave. Example: -c "num_threads=all_cpus","tiled=yes"')
     parser.add_option_group(group)
@@ -146,7 +159,10 @@ def run():
     win = options.win
     # optional options
     mode = options.mode
-    band = options.band
+    try:
+        bands = options.bands.split(',')
+    except:
+        bands = options.bands
     of = options.of
     try:
         co = options.co.split(',')
@@ -154,7 +170,7 @@ def run():
         co = options.co
 
     # sort options to be able to parse them correctly even when user input is mixed up
-    opts = [img, out, win, mode, band, of, co]
+    opts = [img, out, win, mode, bands, of, co]
     index = 0
     for o in range(0,len(opts)):
         # find options not provided with their keyword
