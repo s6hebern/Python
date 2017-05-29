8f18a590173dee404d2b24a65dc585f66d6acb92
s6hebern <hendrik.b88@gmail.com>
2017-05-29 16:16:51 +0200

    bug-fix for floating-point values

--- Python/Raster-Tools/point_sampling.py
+++ Python/Raster-Tools/point_sampling.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os, sys, string, re
+import numpy as np
 from osgeo import ogr, gdal, gdal_array
 from gdalconst import *
-import os
-import string
-import numpy as np
-import re
 
 try:
     import progress_bar as pr
@@ -13,7 +11,7 @@ except:
     pass
 
 
-def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad=0, \
+def point_sampling(raster, shape, bandList=None, dtype=None, winRad=0, \
                    mode='median', noDataValue=None, removeValue=None, precision=None, \
                    names=None):
     """
@@ -24,19 +22,19 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
 
     Use:
 
-    raster (string): the image file (full path and file extension).
+    :param string raster: the image file (full path and file extension).
 
-    shape (string): the shapefile (full path and file extension) containing the 
+    :param string shape: the shapefile (full path and file extension) containing the
             points at which positions the raster shall be sampled.
 
-    bandList (list): a list of integers containing the numbers of the desired 
+    :param list bandList: a list of integers containing the numbers of the desired
             bands to be sampled. Defaults to "None", which means that all bands
             will be used. Counting starts at 1.
 
-    dataType (ogr DataType): the ogr data type of the output fields which shall 
-            be created. Takes only ogr.OFTInteger (default) and ogr.OFTReal.
+    :param string dataType: the desired data type of the output fields which shall
+            be created.
 
-    winRad (integer): the radius of the window around the respective point 
+    :param integer winRad: the radius of the window around the respective point
             position. For example, a value of 4 will create a window of 8x8 
             pixels (4 pixels to each direction). Defaults to 0, which means that 
             the value will be taken from the exact point position.
@@ -48,7 +46,7 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
             window would cross the edges, it will also be resized to the maximum
             possible size.
 
-    mode (string): the statistical value which shall be taken from the window. 
+    :param string mode: the statistical value which shall be taken from the window.
             Possible values are:
                 - 'median' (default)
                 - 'mean'
@@ -56,18 +54,18 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
                 - 'max'
                 - 'majority' (most frequent value, only for integer values)
 
-    noDataValue (integer): the desired nodata-value, if the input image does not
+    :param integer noDataValue: the desired nodata-value, if the input image does not
             have one or it shall be changed for the shapefile.
 
-    removeValue (integer): the nodata-value of the input image, which will be deleted
+    :param integer removeValue: the nodata-value of the input image, which will be deleted
             from the sampling window before calculating the desired statistic.
             Defaults to NONE (which means that no value shall be deleted).
 
-    precision (integer): the precision of the output attribute field, if 
+    :param integer precision: the precision of the output attribute field, if
             floating numbers are desired. If not specified, precision will be 
             set to the length of the maximum value.
 
-    names (list): a list of the field names for the output attribute fields, 
+    :param list names: a list of the field names for the output attribute fields,
             with a maximum length of 10 characters. If not specified, the band 
             names of the raster file will be taken as field names. If they 
             contain characters which are not within (a-z, A-Z, 0-9, _, -) those 
@@ -109,6 +107,7 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
         else:
             b = rst.GetRasterBand(bands[f])
         dt = b.DataType
+        dt = gdal_array.GDALTypeCodeToNumericTypeCode(dt)
         # create field names
         if names == None:
             # get band name and create field name (max length is 10 characters)
@@ -119,18 +118,37 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
         else:
             bName = names[f]
         # get maximum value to set field width (valid values only):
-        maxVal = int(b.ComputeRasterMinMax()[1])
+        maxVal = b.ComputeRasterMinMax(False)[1]
         b = None
+
+        if dtype == None:
+            if type(maxVal) == int:
+                dtype = 'int'
+            elif type(maxVal) == float:
+                dtype = 'float'
+
+        # desired data type
+        if dtype == 'int':
+            dataType = ogr.OFTInteger
+            prec = 0
+        elif dtype == 'float':
+            dataType = ogr.OFTReal
+            prec = len(str(maxVal).split('.')[1])
+        else:
+            print 'Error: dataType has to be either "int" or "float"!'
+            sys.exit(1)
+        if precision != None:
+            prec = precision
+
         # check if fields already exist:
         if bName in lyr.GetFeature(0).keys():
-            pass
+            print 'Warning: field already exists! Values will be overwritten with the pre-defined data type!'
         else:
             field = ogr.FieldDefn(bName, dataType)
-            if precision == None:
-                field.SetWidth(len(str(maxVal)))
-            else:
-                field.SetWidth(len(str(maxVal)) + precision + 1)
-                field.SetPrecision(precision)
+            # set width and precision
+            field.SetWidth(len(str(maxVal)) + 1)
+            field.SetPrecision(prec)
+            # create field
             lyr.CreateField(field)
         # loop through all points and get values:
         for point in xrange(len(points)):
@@ -146,7 +164,7 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
                 noData = 0
             # get value at the desired position (or window):
             if winRad == 0:
-                data = band.ReadAsArray(xOff, yOff, 1, 1)
+                data = band.ReadAsArray(xOff, yOff, 1, 1).astype(dt)
             else:
                 xOff = xOff - winRad
                 yOff = yOff - winRad
@@ -170,10 +188,9 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
                 else:
                     winY = winRad * 2
                 # get data within desired window:
-                window = band.ReadAsArray(xOff, yOff, winX, winY)
+                window = band.ReadAsArray(xOff, yOff, winX, winY).astype(dt)
                 # delete desired removeValue from window:
                 if removeValue != None:
-                    # dt = gdal_array.GDALTypeCodeToNumericTypeCode(dt)
                     dt = type(window[0, 0])
                     window = window[window != dt(removeValue)]
                 if window.size == 0:
@@ -191,16 +208,16 @@ def point_sampling(raster, shape, bandList=None, dataType=ogr.OFTInteger, winRad
                     elif mode == 'majority':
                         data = np.bincount(window.flatten()).argmax()
             # convert to desired data type:
-            if dataType == ogr.OFTInteger and data != None:
+            if dtype == 'int' and data != None:
                 val = int(data)
-            elif dataType == ogr.OFTInteger and data == None:
+            elif dtype == 'int' and data == None:
                 val = int(noData)
-            elif dataType == ogr.OFTReal and data != None:
+            elif dtype == 'float' and data != None:
                 val = float(data)
-            elif dataType == ogr.OFTReal and data == None:
+            elif dtype == 'float' and data == None:
                 val = float(noData)
             else:
-                raise (Warning('Invalid Data Type assigned! Function takes only ogr.OFTInteger and ogr.OFTReal'))
+                raise (Warning('Invalid Data Type assigned! Function takes only "int" or "float'))
             # set value:
             feat = lyr.GetFeature(point)
             feat.SetField(bName, val)
